da85c3bd33aa13a6f803d3b9ff1d5f74297d0514
Yoel <yobelb8@gmail.com>
2024-10-29 10:39:55 +0000

    Añadido paso final en la 3-7 y comenzando 3-8

--- Yoel_3-7.py
+++ Yoel_3-7.py
@@ -1,31 +1,36 @@
 # Define la clase Car en Python 3. La clase tendrá como atributos “matrícula” (numérica) y “color”. 
 # Crea un método imprimir y, además, dos métodos adicionales que elijas.
-# En segundo lugar, haz que el programa pida un número “n” por teclado y se creen “n” instancias de la clase, donde cada instancia:
-# Cada “matrícula” tendrá un número consecutivo desde 1 hasta “n”.
-# El “color” será para cada instancia un color aleatorio obtenido de esta lista: ["red", "white", "black", "pink", "blue"].
-# Finalmente, el programa deberá imprimir los valores de las 10 primeras instancias. En caso de que “n” sea menor que 10, solo se imprimirán “n” instancias.
 
 import random
 
+# El “color” será para cada instancia un color aleatorio obtenido de esta lista: ["red", "white", "black", "pink", "blue"].
 colores = ["red", "white", "black", "pink", "blue"]
 
 class Car:
     def __init__(self, matricula, color):
         self.matricula = matricula
         self.color = color
+
     def arrancar(self):
         print(f"El coche con matrícula {self.matricula} y color {self.color} ha arrancado.")
+
     def frenar(self):
         print(f"El coche con matrícula {self.matricula} y color {self.color} ha frenado.")
+
     def imprimir(self):
         print(f"Matrícula: {self.matricula}, Color: {self.color}")
 
 n = int(input("Introduce el número de coches a crear: "))
 
+# En segundo lugar, haz que el programa pida un número “n” por teclado y se creen “n” instancias de la clase, donde cada instancia:
+# Cada “matrícula” tendrá un número consecutivo desde 1 hasta “n”.
 coches = []
-
-# +1 por el range especial de python
 for i in range(1, n + 1):
     color_aleatorio = random.choice(colores)
     coche = Car(matricula=i, color=color_aleatorio)
     coches.append(coche)
+
+# Finalmente, el programa deberá imprimir los valores de las 10 primeras instancias. En caso de que “n” sea menor que 10, solo se imprimirán “n” instancias.
+cantidad_a_imprimir = min(n, 10)
+for coche in coches[:cantidad_a_imprimir]:
+    coche.imprimir()
---
+++ Yoel_3-8.py
@@ -0,0 +1,15 @@
+# Explica y pon un ejemplo sencillo de una función lambda en Python 3.
+# Una función lambda es una función pequeña y anónima que se define en una sola línea. Se utiliza para operaciones simples que no necesitan un nombre explícito
+
+# Una funcion lambda es como una funcion pero muy corta, casi siempre se define en una sola linea y se utiliza para hacer operaciones simples
+
+suma = lambda x, y: x + y
+
+resultado = suma(3, 2)
+print(resultado)
+
+# Después, pon un ejemplo que utilice las funciones de Python:
+# Usando “map()” (y “list()” e “int()” como apoyo para convertir a lista y a números enteros), 
+# lee desde teclado una cadena de texto formada por números separados por espacios y transfórmala en una lista formada por números enteros.
+# Usando “filter()”, elimina de la cadena anterior los números menores que 10.
+# Con la cadena resultante y usando “reduce()”, devuelve la multiplicación de los elementos de la lista.
